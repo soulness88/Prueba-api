4e2e027352c30d28bc658433915844e3487f007b
soulness88 <106523827+soulness88@users.noreply.github.com>
2025-02-17 13:21:54 +0100

    app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+def checker(name):
+  return "heheh"
+
+@app.route('/page', methods=['GET'])
+def new():
+  name = request.args-get('name'=
+  if name == 'DHRUV':
+    checker(name)
+  return jsonify({
+    'name': 'Dhruv'
+  })
